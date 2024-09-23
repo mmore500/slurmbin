79268a2b98bb21c501902f7bb94c2fcaecd164b3
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-09-23 13:12:13 -0400

    Fix gz -> gzip

--- ptargz.sh
+++ ptargz.sh
@@ -7,5 +7,5 @@ total_size=$(du -sbc "${@:2}" | tail -n 1 | awk '{print $1}')
 echo "Total size of files to be archived: $total_size bytes"
 
 # Do the tar and gzip
-tar cf - "${@:2}" -P | singularity run docker://ghcr.io/mmore500/pv -s $total_size | gz  > $1
+tar cf - "${@:2}" -P | singularity run docker://ghcr.io/mmore500/pv -s $total_size | gzip  > $1
 
