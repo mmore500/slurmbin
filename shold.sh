b32a5bb5c97260814058978b882a7232fdcc9a5e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-10-11 00:12:17 -0400

    Update shold.sh

--- shold.sh
+++ shold.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-squeue -h -u $USER --format "scontrol hold job %i"
-squeue -h -u $USER --format "scontrol hold job %i" | sh
+squeue -h -u $USER --format "scontrol hold %i"
+squeue -h -u $USER --format "scontrol hold %i" | sh
 
