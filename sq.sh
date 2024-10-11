423c03f43ccd167fdf2a629cda184ed3e4f14e54
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-10-11 11:18:45 -0400

    Load quota from bashrc

--- sq.sh
+++ sq.sh
@@ -29,4 +29,4 @@ printf "%-30s %d\n" "Held jobs:" $held_count
 printf "%-30s %d\n" "Pending jobs:" $pending_count
 printf "%-30s %d\n" "Running jobs:" $running_count
 
-echo "Quota: " $(bash -c "quota" | grep -o '[^ ]\{2,\}%' | tr '\n' ' ')
+echo "Quota: " $(bash -i -c "quota" | grep -o '[^ ]\{2,\}%' | tr '\n' ' ')
